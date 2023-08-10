03184017e62e8d8c122cd032e468f782aeac8b8a
Mehedihasan023 <mh20973@gmail.com>
2023-08-11 03:41:59 +0600

    private route implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,25 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from './providers/AuthProviders';
 
 const Header = () => {
-    const { user } = useContext(AuthContext);
-
+    const { user, logOut } = useContext(AuthContext);
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.log(error))
+    }
     return (
         <div>
             <div className="navbar bg-primary text-primary-content">
                 <a className="btn btn-ghost normal-case text-xl">Auth Master</a>
                 <Link className="btn btn-ghost normal-case text-xl" to='/'>Home</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to='/orders'>Orders</Link>
                 <Link className="btn btn-ghost normal-case text-xl" to='/login'>Login</Link>
                 <Link className="btn btn-ghost normal-case text-xl" to='/register'>Register</Link>
                 {
                     user ?
                         <>
                             <span>{user.email}</span>
-                            <button className="btn btn-xs">Sign out</button>
+                            <button onClick={handleLogOut} className="btn btn-xs">Sign out</button>
                         </> : <Link to='/login'>login</Link>
 
                 }
